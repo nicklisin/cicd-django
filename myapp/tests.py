137f6e38816c1dde3e5692355ee7ad0acce2367c
lisin <lisinnic@gmail.com>
2023-06-02 04:06:54 -0700

    Basic tests added

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Page
 
-# Create your tests here.
+class ModelsTest(TestCase):
+    def setUp(self) -> None:
+        self.page = Page.objects.create(name="TestPage")
+        return super().setUp()
+    
+    def testPage(self):
+        p = self.page
+        self.assertTrue(isinstance(p, Page))
+        self.assertEqual(str(p), "TestPage")
