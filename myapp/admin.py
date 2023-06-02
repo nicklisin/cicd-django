4f346c87b195abef1a775ed94c2fdedd96e030db
lisin <lisinnic@gmail.com>
2023-06-01 21:31:43 -0700

    Ititial commit

---
+++ myapp/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from myapp.models import Page
+
+admin.site.register(Page)
---
+++ myapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Page(models.Model):
+    name = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
