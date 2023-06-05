685473e65b781daaf50b159f1853b47e394de916
lisin <lisinnic@gmail.com>
2023-06-05 04:34:03 -0700

    linter fix

--- config/settings/base_settings.py
+++ config/settings/base_settings.py
@@ -3,7 +3,9 @@ from dotenv import load_dotenv
 import os
 
 
-load_dotenv() # take environment variables from .env.
+# take environment variables from .env.
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
