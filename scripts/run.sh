7327f3b143357dd71637f1f9ec545840cfa069cc
lisin <lisinnic@gmail.com>
2023-06-24 21:58:12 -0700

    Gunicorn in run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,4 +10,4 @@ whoami
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+gunicorn config.wsgi:application --bind 0.0.0.0:8000
