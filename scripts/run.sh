8877bc0642635660a348b9a262f23cf48bc785c5
lisin <lisinnic@gmail.com>
2023-06-26 23:06:20 -0700

    PGDB and Nginx added

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,4 +10,4 @@ whoami
 python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
-gunicorn config.wsgi:application --bind 0.0.0.0:8000
+gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 2 --threads 4
